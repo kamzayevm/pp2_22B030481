0e2bb6af1044f2591b225fdaf06fce11a61f83a2
kamzayevm <122873632+kamzayevm@users.noreply.github.com>
2023-03-05 22:52:29 +0600

    Delete labka6/functions directory

--- labka6/functions/task1.py
+++
@@ -1,4 +0,0 @@
-from functools import reduce
-numbers = [1, 2, 3, 4, 5]
-res = reduce(lambda x, y: x * y, numbers)
-print("Product of all the numbers:", res)
--- labka6/functions/task3.py
+++
@@ -1,8 +0,0 @@
-def pal(text):
-    x = text[::-1]
-    if text == x:
-        print("Yes")
-    else:
-        print("No")
-c = input("Введите число: ")
-pal(c)
